a858934564f2b76575c017e67f563a820aea5438
Andrew Benson <andrewbenson123@gmail.com>
2020-11-20 17:51:27 -0500

    Current and Comparison php files

--- comparison.php
+++ comparison.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
 <html>
     <head>
         <meta charset="utf-8">
@@ -12,13 +8,133 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="">
         <?php include 'template/includes.php';?>
+		<?php include 'template/navigation.php';?>
+		
+			<style>
+		.card-title{
+			font-size: 300%;
+
+		}
+
+
+		.current-list-item{
+			font-size: 200%
+				
+		}
+
+		.card-subtitle{
+			font-size:250%
+			
+		}
+
+		.card {
+				width: 33%;
+				margin: 0 auto; /* Added */
+				float: left; /* Added */
+				padding: 10px 3% 0px 3%;
+				margin-bottom: 10px; /* Added */
+				display:inline-block;
+		}
+
+		.form-inline {
+		  display: flex;
+		  flex-flow: row wrap;
+		  align-items: center;
+		}
+		
+		</style>
     </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-        
-        <script src="" async defer></script>
-        to be done by Andrew
+    <body>  
+        <script src="" async defer></script>       
+		<!--City 1 card-->
+<div class="card center">
+
+	<div class="input" method="post">
+		  
+		  <label for="city">City Name:</label><br>
+		  <input type="text" id="city1" name="city1"><br><br>
+		  <button onclick="searchCity('1')">Search</button>
+
+	</div>
+
+	<div class ="card-body">
+		<h5 class = "card-title" id="name1"></h5>
+		<h6 class="card-subtitle mb-2 text-muted" id="desc1" style="float:left;"></h6>
+		<img  id="icon1" src="" width="15%" height="auto" alt="Card image cap">
+	</div>	
+	<div style="clear:left;">
+	<ul class="list-group list-group-flush">
+		<li class="list-group-item current-list-item" id="temp1"></li>
+		<li class="list-group-item current-list-item" id="feels_like1"></li>
+		<li class="list-group-item current-list-item" id ="humidity1"></li>
+	</ul>
+	</div>
+	<div class="card-body">
+		<a href="https://www.cs.ryerson.ca/~arbenson/Project/weather/current.php" class="card-link">Current </a>
+		<a href="#" class="card-link"> Link2</a>
+	</div>
+</div>
+
+
+		<!--City 2 card-->
+<div class="card center">
+
+	<div class="input" method="post">
+		  
+		  <label for="city">City Name:</label><br>
+		  <input type="text" id="city2" name="city2"><br><br>
+		  <button onclick="searchCity('2')">Search</button>
+
+	</div>
+
+	<div class ="card-body">
+		<h5 class = "card-title" id="name2">-</h5>
+		<h6 class="card-subtitle mb-2 text-muted" id="desc2" style="float:left;">-</h6>
+		<img  id="icon2" src="http://2.bp.blogspot.com/-UIjLsu00T6w/UZhz5NuKjnI/AAAAAAAADYw/z6zyAVj_nG4/s1600/white-square-872x844.jpg" width="15%" height="auto" alt="Card image cap">
+	</div>	
+	<div style="clear:left;">
+	<ul class="list-group list-group-flush">
+		<li class="list-group-item current-list-item" id="temp2">-</li>
+		<li class="list-group-item current-list-item" id="feels_like2">-</li>
+		<li class="list-group-item current-list-item" id ="humidity2">-</li>
+	</ul>
+	</div>
+	<div class="card-body">
+		<a href="#" class="card-link">Link1 </a>
+		<a href="#" class="card-link"> Link2</a>
+	</div>
+</div>		
+		
+				<!--City 3 card-->
+<div class="card center">
+
+	<div class="input" method="post">
+		  
+		  <label for="city">City Name:</label><br>
+		  <input type="text" id="city3" name="city3"><br><br>
+		  <button onclick="searchCity('3')">Search</button>
+
+	</div>
+
+	<div class ="card-body">
+		<h5 class = "card-title" id="name3">-</h5>
+		<h6 class="card-subtitle mb-2 text-muted" id="desc3" style="float:left;">-</h6>
+		<img  id="icon3" src="http://2.bp.blogspot.com/-UIjLsu00T6w/UZhz5NuKjnI/AAAAAAAADYw/z6zyAVj_nG4/s1600/white-square-872x844.jpg" width="15%" height="auto" alt="Card image cap">
+	</div>	
+	<div style="clear:left;">
+	<ul class="list-group list-group-flush">
+		<li class="list-group-item current-list-item" id="temp3">-</li>
+		<li class="list-group-item current-list-item" id="feels_like3">-</li>
+		<li class="list-group-item current-list-item" id ="humidity3">-</li>
+	</ul>
+	</div>
+	<div class="card-body">
+		<a href="#" class="card-link">Link1 </a>
+		<a href="#" class="card-link"> Link2</a>
+	</div>
+</div>
+		
+		
+<script src="js/comparison.js"></script>		
     </body>
 </html>
--- current.php
+++ current.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
 <html>
     <head>
         <meta charset="utf-8">
@@ -11,14 +7,96 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="">
+		
         <?php include 'template/includes.php';?>
+	
+		<?php include 'template/navigation.php';?>
+		<style>
+		.card-title{
+			font-size: 300%;
+
+		}
+
+
+		.current-list-item{
+			font-size: 200%
+				
+		}
+
+		.card-subtitle{
+			font-size:250%
+			
+		}
+
+		.card {
+				width: 33%;
+				margin: 0 auto; /* Added */
+				float: left; /* Added */
+				padding: 10px 3% 0px 3%;
+				margin-bottom: 10px; /* Added */
+				display:inline-block;
+		}
+
+		.form-inline {
+		  display: flex;
+		  flex-flow: row wrap;
+		  align-items: center;
+		}
+		
+		</style>
+		
+		
     </head>
     <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-        
-        <script src="" async defer></script>
-        this current page to be done by Andrew
+    <script src="" async defer></script>
+		
+		
+		
+		
+<!--Search Bar -->
+<div class="input"  style="float:right;">
+
+	  <label for="city">City Name:</label><br>
+	  <input type="text" id="city" name="city"><br><br>
+	  <button onclick="searchCity()">Search</button>
+
+</div>
+
+
+<!--Current Weather card -->
+<div class="card center">
+	<div class ="card-body">
+		<h5 class = "card-title" id="name"></h5>
+		<h6 class="card-subtitle mb-2 text-muted" style="float:left;" id="description"></h6>
+		<img id="icon" src="" width="15%;" height="auto" alt="Card image cap">
+	</div>	
+	<div style="clear:left;">
+	<ul class="list-group list-group-flush">
+		<li class="list-group-item current-list-item " id ="temp"></li>
+		<li class="list-group-item current-list-item" id="feels_like"></li>
+		<li class="list-group-item current-list-item" id="humidity"></li>
+	</ul>
+	</div>
+	<div class="card-body">
+		<a href="comparison.php" class="card-link">Comparison </a>
+		<a href="#" class="card-link"> Weekly Forecast</a>
+	</div>
+</div>
+	
+<div class="card center">
+<ul class="list-group list-group-flush">
+		<li class="list-group-item current-list-item" id="sunrise">Sunrise: <?php echo date("H:i:s A", ($currentWeather->main->sunrise)); ?> UTC </li>
+		<li class="list-group-item current-list-item" id="sunset">Sunset:  <?php echo date("H:i:s A",($currentWeather->main->sunset)); ?> UTC</li>
+		<li class="list-group-item current-list-item" id="cloudCoverage">Cloud Coverage: <?php echo $currentWeather->main->clouds; ?>%  </li>
+		<li class="list-group-item current-list-item" id="visibility">Visibility: <?php echo ($currentWeather->main->visibility)/1000; ?>km </li>
+		<li class="list-group-item current-list-item" id="wind_speed">Wind Speed: <?php echo $currentWeather->main->wind_speed; ?>m/s  </li>
+		<li class="list-group-item current-list-item" id="wind_degree">Wind Degree: <?php echo $currentWeather->main->wind_deg; ?>° </li>
+
+	</ul>
+
+</div>		
+		
+	
+<script src="js/currentWeather.js"></script>	
     </body>
 </html>
