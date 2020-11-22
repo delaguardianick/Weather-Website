5d1b0a9504189dd155b4642803d3d1933ce260a5
Sagar Singh <singh13sagar@gmail.com>
2020-11-22 18:21:15 -0500

    Added alerts

--- alerts.php
+++ alerts.php
@@ -12,13 +12,29 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="">
         <?php include 'template/includes.php';?>
+        <script src="js/alerts.js"></script>
     </head>
     <body>
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-        
+        <?php include 'template/navigation.php';?>
         <script src="" async defer></script>
-        To be done by Sagar
+        <div class="main-content border border-dark mt-4">
+            <div class="city-content ml-2">
+                <h3>City: <span id="citySpan"></span></h3>
+                <h3>As of: <span id="asOf"></span></h3>
+            </div>
+            <div id="dataSection">
+                <div id="info">
+                </div>
+                <div id="alert">
+                    <h4 class="bg-primary text-white mr-3 ml-3 mt-5 mb-5 text-center">Please select a city</h4>
+                </div>
+                <div id="noData" class="hidden">
+                    <h4 class="bg-success text-white mr-3 ml-3 mt-5 mb-5 text-center">No alerts available.</h4>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
