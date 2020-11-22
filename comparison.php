7e8ede1c941bc7351d473b7807624179c0464860
Andrew Benson <andrewbenson123@gmail.com>
2020-11-22 15:49:45 -0500

    Comparison and Current files
    Updated some tags for css

--- comparison.php
+++ comparison.php
@@ -6,134 +6,105 @@
         <title>City Comparison</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="css/andrew.css">
         <?php include 'template/includes.php';?>
 		<?php include 'template/navigation.php';?>
-		
-			<style>
-		.card-title{
-			font-size: 300%;
-
-		}
-
-
-		.current-list-item{
-			font-size: 200%
-				
-		}
-
-		.card-subtitle{
-			font-size:250%
-			
-		}
-
-		.card {
-				width: 33%;
-				margin: 0 auto; /* Added */
-				float: left; /* Added */
-				padding: 10px 3% 0px 3%;
-				margin-bottom: 10px; /* Added */
-				display:inline-block;
-		}
-
-		.form-inline {
-		  display: flex;
-		  flex-flow: row wrap;
-		  align-items: center;
-		}
-		
-		</style>
     </head>
     <body>  
         <script src="" async defer></script>       
+
+
+
+
+<div id="main-container" class="overflow-auto">
+	
+	<div id="sub-container" class="container bg-light ">
 		<!--City 1 card-->
-<div class="card center">
+		<div id="header-div">
+			<h1 class="title d-flex justify-content-center">City Comparison</h1>
+		</div>
+		<div class="card-deck">
+		<div class="card center">
 
-	<div class="input" method="post">
-		  
-		  <label for="city">City Name:</label><br>
-		  <input type="text" id="city1" name="city1"><br><br>
-		  <button onclick="searchCity('1')">Search</button>
+			<div class="input" method="post">
+				  
+				  <input class="inline" type="text" id="city1" name="city1" placeholder="Search for a city" >
+				  <button class="inline" onclick="searchCity('1')">Search</button>
 
-	</div>
+			</div>
 
-	<div class ="card-body">
-		<h5 class = "card-title" id="name1"></h5>
-		<h6 class="card-subtitle mb-2 text-muted" id="desc1" style="float:left;"></h6>
-		<img  id="icon1" src="" width="15%" height="auto" alt="Card image cap">
-	</div>	
-	<div style="clear:left;">
-	<ul class="list-group list-group-flush">
-		<li class="list-group-item current-list-item" id="temp1"></li>
-		<li class="list-group-item current-list-item" id="feels_like1"></li>
-		<li class="list-group-item current-list-item" id ="humidity1"></li>
-	</ul>
-	</div>
-	<div class="card-body">
-		<a href="https://www.cs.ryerson.ca/~arbenson/Project/weather/current.php" class="card-link">Current </a>
-		<a href="#" class="card-link"> Link2</a>
-	</div>
-</div>
+			<div class ="card-body">
+				<h2 class = "card-title city-title" id="name1"></h2>
+				<h3 class="card-subtitle mb-2 text-muted city-title" id="desc1"></h3>
+				<img class="weather-icon" id="icon1" src=""  alt="Card image cap">
+			</div>	
+			<div style="clear:left;">
+			<ul class="list-group list-group-flush">
+				<li class="list-group-item current-list-item card-list" id="temp1"></li>
+				<li class="list-group-item current-list-item card-list" id="feels_like1"></li>
+				<li class="list-group-item current-list-item card-list" id ="humidity1"></li>
+			</ul>
+			</div>
+		</div>
 
 
-		<!--City 2 card-->
-<div class="card center">
+				<!--City 2 card-->
+		<div class="card center">
 
-	<div class="input" method="post">
-		  
-		  <label for="city">City Name:</label><br>
-		  <input type="text" id="city2" name="city2"><br><br>
-		  <button onclick="searchCity('2')">Search</button>
+			<div class="input" method="post">
+				  
+				 
+				  <input class="inline" type="text" id="city2" name="city2" placeholder="Search for a city">
+				  <button class="inline" onclick="searchCity('2')">Search</button>
 
-	</div>
+			</div>
 
-	<div class ="card-body">
-		<h5 class = "card-title" id="name2">-</h5>
-		<h6 class="card-subtitle mb-2 text-muted" id="desc2" style="float:left;">-</h6>
-		<img  id="icon2" src="http://2.bp.blogspot.com/-UIjLsu00T6w/UZhz5NuKjnI/AAAAAAAADYw/z6zyAVj_nG4/s1600/white-square-872x844.jpg" width="15%" height="auto" alt="Card image cap">
-	</div>	
-	<div style="clear:left;">
-	<ul class="list-group list-group-flush">
-		<li class="list-group-item current-list-item" id="temp2">-</li>
-		<li class="list-group-item current-list-item" id="feels_like2">-</li>
-		<li class="list-group-item current-list-item" id ="humidity2">-</li>
-	</ul>
-	</div>
-	<div class="card-body">
-		<a href="#" class="card-link">Link1 </a>
-		<a href="#" class="card-link"> Link2</a>
-	</div>
-</div>		
-		
-				<!--City 3 card-->
-<div class="card center">
+			<div class ="card-body">
+				<h2 class = "card-title city-title" id="name2">-</h2>
+				<h3 class="card-subtitle mb-2 text-muted city-title " id="desc2">-</h3>
+				<img class="weather-icon" class="inline"  id="icon2" src="http://2.bp.blogspot.com/-UIjLsu00T6w/UZhz5NuKjnI/AAAAAAAADYw/z6zyAVj_nG4/s1600/white-square-872x844.jpg"   alt="Card image cap">
+			</div>	
+			<div style="clear:left;">
+			<ul class="list-group list-group-flush">
+				<li class="list-group-item current-list-item card-list" id="temp2">-</li>
+				<li class="list-group-item current-list-item card-list" id="feels_like2">-</li>
+				<li class="list-group-item current-list-item card-list" id ="humidity2">-</li>
+			</ul>
+			</div>
+		</div>		
+				
+						<!--City 3 card-->
+		<div class="card center">
 
-	<div class="input" method="post">
-		  
-		  <label for="city">City Name:</label><br>
-		  <input type="text" id="city3" name="city3"><br><br>
-		  <button onclick="searchCity('3')">Search</button>
+			<div class="input" method="post">
+				  
+				  
+				  <input class="inline" type="text" id="city3" name="city3" placeholder="Search for a city">
+				  <button class="inline" onclick="searchCity('3')">Search</button>
 
-	</div>
+			</div>
 
-	<div class ="card-body">
-		<h5 class = "card-title" id="name3">-</h5>
-		<h6 class="card-subtitle mb-2 text-muted" id="desc3" style="float:left;">-</h6>
-		<img  id="icon3" src="http://2.bp.blogspot.com/-UIjLsu00T6w/UZhz5NuKjnI/AAAAAAAADYw/z6zyAVj_nG4/s1600/white-square-872x844.jpg" width="15%" height="auto" alt="Card image cap">
-	</div>	
-	<div style="clear:left;">
-	<ul class="list-group list-group-flush">
-		<li class="list-group-item current-list-item" id="temp3">-</li>
-		<li class="list-group-item current-list-item" id="feels_like3">-</li>
-		<li class="list-group-item current-list-item" id ="humidity3">-</li>
-	</ul>
-	</div>
-	<div class="card-body">
-		<a href="#" class="card-link">Link1 </a>
-		<a href="#" class="card-link"> Link2</a>
+			<div class ="card-body">
+				<h2 class = "card-title city-title" id="name3">-</h2>
+				<h3 class="card-subtitle mb-2 text-muted city-title" id="desc3" >-</h3>
+				<img class="weather-icon" id="icon3" src="http://2.bp.blogspot.com/-UIjLsu00T6w/UZhz5NuKjnI/AAAAAAAADYw/z6zyAVj_nG4/s1600/white-square-872x844.jpg" alt="Card image cap">
+			</div>	
+			<div style="clear:left;">
+			<ul class="list-group list-group-flush">
+				<li class="list-group-item current-list-item card-list" id="temp3">-</li>
+				<li class="list-group-item current-list-item card-list" id="feels_like3">-</li>
+				<li class="list-group-item current-list-item card-list" id ="humidity3">-</li>
+			</ul>
+			</div>
+		</div>
+		</div>
 	</div>
 </div>
-		
+
+
+
+
+	
 		
 <script src="js/comparison.js"></script>		
     </body>
--- current.php
+++ current.php
@@ -6,97 +6,83 @@
         <title>Current Weather Data</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="css/andrew.css">
 		
         <?php include 'template/includes.php';?>
-	
-		<?php include 'template/navigation.php';?>
-		<style>
-		.card-title{
-			font-size: 300%;
-
-		}
-
-
-		.current-list-item{
-			font-size: 200%
-				
-		}
-
-		.card-subtitle{
-			font-size:250%
-			
-		}
-
-		.card {
-				width: 33%;
-				margin: 0 auto; /* Added */
-				float: left; /* Added */
-				padding: 10px 3% 0px 3%;
-				margin-bottom: 10px; /* Added */
-				display:inline-block;
-		}
-
-		.form-inline {
-		  display: flex;
-		  flex-flow: row wrap;
-		  align-items: center;
-		}
-		
-		</style>
-		
-		
     </head>
     <body>
     <script src="" async defer></script>
+	
+
+ <?php include 'template/navigation.php';?>
 		
 		
-		
-		
-<!--Search Bar -->
+<!--Search Bar
 <div class="input"  style="float:right;">
 
 	  <label for="city">City Name:</label><br>
 	  <input type="text" id="city" name="city"><br><br>
 	  <button onclick="searchCity()">Search</button>
 
-</div>
+</div> -->
 
+<!--Current Weather card 
 
-<!--Current Weather card -->
-<div class="card center">
-	<div class ="card-body">
-		<h5 class = "card-title" id="name"></h5>
-		<h6 class="card-subtitle mb-2 text-muted" style="float:left;" id="description"></h6>
-		<img id="icon" src="" width="15%;" height="auto" alt="Card image cap">
+<div id="header-div">
+		<h6 class="title d-flex justify-content-center">Current Weather</h6>
+	</div>
+
+-->
+<div id="main-container">
+	
+	<div id="sub-container" class="container bg-light">
+		<div id="header-div">
+			<h1 class="title d-flex justify-content-center">Current Weather</h1>
+		</div>
+		<div class="card-deck">
+			<div class="card " id="card">
+				<div class ="card-body">
+					<h2 class = "card-title city-title" id="name"></h2>
+					
+					
+					<h2 class="card-subtitle mb-2 text-muted city-title" id="description" ></h2>
+					<img id="icon" src="" width="auto;" height="auto" alt="Card image cap" >
+					
+				</div>	
+				<div>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item current-list-item card-list " id ="temp"></li>
+					<li class="list-group-item current-list-item card-list " id="feels_like"></li>
+					<li class="list-group-item current-list-item card-list " id="humidity"></li>
+					
+					<!--<li class="list-group-item current-list-item card-list" id="sunrise"></li>
+					<li class="list-group-item current-list-item card-list" id="sunset"></li>
+					<li class="list-group-item current-list-item card-list" id="cloudCoverage"></li>
+					<li class="list-group-item current-list-item card-list" id="visibility"></li>
+					<li class="list-group-item current-list-item card-list" id="wind_speed"></li>
+					<li class="list-group-item current-list-item card-list" id="wind_degree"></li>
+
+				</ul>-->
+				</div>
+
+			</div>
+				
+			<div class="card" id="card">
+			
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item current-list-item card-list" id="sunrise">Sunrise: <?php echo date("H:i:s A", ($currentWeather->main->sunrise)); ?> UTC </li>
+					<li class="list-group-item current-list-item card-list" id="sunset">Sunset:  <?php echo date("H:i:s A",($currentWeather->main->sunset)); ?> UTC</li>
+					<li class="list-group-item current-list-item card-list" id="cloudCoverage">Cloud Coverage: <?php echo $currentWeather->main->clouds; ?>%  </li>
+					<li class="list-group-item current-list-item card-list" id="visibility">Visibility: <?php echo ($currentWeather->main->visibility)/1000; ?>km </li>
+					<li class="list-group-item current-list-item card-list" id="wind_speed">Wind Speed: <?php echo $currentWeather->main->wind_speed; ?>m/s  </li>
+					<li class="list-group-item current-list-item card-list" id="wind_degree">Wind Degree: <?php echo $currentWeather->main->wind_deg; ?>° </li>
+
+				</ul>	
+			</div>		
+		</div>	
 	</div>	
-	<div style="clear:left;">
-	<ul class="list-group list-group-flush">
-		<li class="list-group-item current-list-item " id ="temp"></li>
-		<li class="list-group-item current-list-item" id="feels_like"></li>
-		<li class="list-group-item current-list-item" id="humidity"></li>
-	</ul>
-	</div>
-	<div class="card-body">
-		<a href="comparison.php" class="card-link">Comparison </a>
-		<a href="#" class="card-link"> Weekly Forecast</a>
-	</div>
-</div>
-	
-<div class="card center">
-<ul class="list-group list-group-flush">
-		<li class="list-group-item current-list-item" id="sunrise">Sunrise: <?php echo date("H:i:s A", ($currentWeather->main->sunrise)); ?> UTC </li>
-		<li class="list-group-item current-list-item" id="sunset">Sunset:  <?php echo date("H:i:s A",($currentWeather->main->sunset)); ?> UTC</li>
-		<li class="list-group-item current-list-item" id="cloudCoverage">Cloud Coverage: <?php echo $currentWeather->main->clouds; ?>%  </li>
-		<li class="list-group-item current-list-item" id="visibility">Visibility: <?php echo ($currentWeather->main->visibility)/1000; ?>km </li>
-		<li class="list-group-item current-list-item" id="wind_speed">Wind Speed: <?php echo $currentWeather->main->wind_speed; ?>m/s  </li>
-		<li class="list-group-item current-list-item" id="wind_degree">Wind Degree: <?php echo $currentWeather->main->wind_deg; ?>° </li>
-
-	</ul>
-
-</div>		
-		
-	
-<script src="js/currentWeather.js"></script>	
+</div>	
+    <script src="js/currentWeather.js"></script>
+ <!--    <script src="js/template.js"></script> -->
     </body>
 </html>
