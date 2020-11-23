380885559dee1caa36cb887999ceb7f9e1523361
DTran990 <74727093+DTran990@users.noreply.github.com>
2020-11-23 14:48:26 -0500

    Updated term.php

--- term.php
+++ term.php
@@ -14,10 +14,119 @@
         <?php include 'template/includes.php';?>
     </head>
     <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-        
+        <?php include 'template/navigation.php';?>
+        <div id="term-container">
+          <div id = "term-info">
+            <h1 id="term-head">Terminology</h1>
+            <p id="term-description"> This page provides you with the definitions of weather terminology that will be used on this site. Feel free to check this page out whenever you are unsure of what something means.</p>
+          </div>
+          <div id="terms">
+            <div class="term">
+              <h4 class="term-name">Temperature</h4>
+              <p>
+                <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample">
+                  Definition
+                </a>
+              </p>
+              <div class="collapse" id="collapseExample1">
+                <div class="card card-body">
+                  The measure of how hot or cold the atmosphere is.
+                </div>
+              </div>
+            </div>
+            <div class="term">
+              <h4 class="term-name">Humidity</h4>
+              <p>
+                <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample">
+                  Definition
+                </a>
+              </p>
+              <div class="collapse" id="collapseExample1">
+                <div class="card card-body">
+                  The state of the air when it is full of water vapor.
+                </div>
+              </div>
+            </div>
+            <div class="term">
+              <h4 class="term-name">Pressure</h4>
+              <p>
+                <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample">
+                  Definition
+                </a>
+              </p>
+              <div class="collapse" id="collapseExample1">
+                <div class="card card-body">
+                  The force exerted on a surface by the air above it as gravity pulls it to Earth. Measured in kPa.
+                </div>
+              </div>
+            </div>
+            <div class="term">
+              <h4 class="term-name">UV Index (UV)</h4>
+              <p>
+                <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample">
+                  Definition
+                </a>
+              </p>
+              <div class="collapse" id="collapseExample1">
+                <div class="card card-body">
+                  The amount of skin-damaging UV radiation reaching the earth's surface.
+                </div>
+              </div>
+            </div>
+            <div class="term">
+              <h4 class="term-name">Visibility</h4>
+              <p>
+                <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample">
+                  Definition
+                </a>
+              </p>
+              <div class="collapse" id="collapseExample1">
+                <div class="card card-body">
+                  The measure of distance at which an object or light could be recognized.
+                </div>
+              </div>
+            </div>
+            <div class="term">
+              <h4 class="term-name">Wind Speed</h4>
+              <p>
+                <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample">
+                  Definition
+                </a>
+              </p>
+              <div class="collapse" id="collapseExample1">
+                <div class="card card-body">
+                  The measure of how fast air is moving at a certain point of time.
+                </div>
+              </div>
+            </div>
+            <div class="term">
+              <h4 class="term-name">Wind Gust</h4>
+              <p>
+                <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample">
+                  Definition
+                </a>
+              </p>
+              <div class="collapse" id="collapseExample1">
+                <div class="card card-body">
+                  The measure of the sudden increase in wind speed.
+                </div>
+              </div>
+            </div>
+            <div class="term">
+              <h4 class="term-name">Wind Degree</h4>
+              <p>
+                <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample">
+                  Definition
+                </a>
+              </p>
+              <div class="collapse" id="collapseExample1">
+                <div class="card card-body">
+                  The direction of which air is moving.
+                </div>
+              </div>
+            </div>
+          </div>
+        <?php include 'template/footer.php';?>
         <script src="" async defer></script>
     </body>
 </html>
