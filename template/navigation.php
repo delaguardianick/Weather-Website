241cce6b75dff62478549687d7484a8e00f0acc4
Sagar Singh <singh13sagar@gmail.com>
2020-11-26 15:07:07 -0500

    couple of css changes

--- template/navigation.php
+++ template/navigation.php
@@ -1,6 +1,6 @@
 <?php 
 echo '
-<nav class="navbar navbar-expand-lg navbar-light text-white d-flex" style="background:#1D3557; ">
+<nav class="navbar navbar-expand-lg navbar-light text-white d-flex">
     <a class="navbar-brand text-white" href="#"> <img src="https://images-na.ssl-images-amazon.com/images/I/61nuuPxUvaL.png" alt="" id="logo"> Weather</a>
     <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -11,7 +11,7 @@ echo '
           <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Forecast
           </a>
-          <div style="background:#264672;" class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
             <a class="dropdown-item text-white" href="current.php">Current</a>
             <a class="dropdown-item text-white" href="minute.php">Minutely</a>
             <a class="dropdown-item text-white" href="hourly.php">Hourly</a>
@@ -21,7 +21,7 @@ echo '
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Info</a>
-          <div style="background:#264672; " class="  text-white dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <div class="  text-white dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
             <a class="dropdown-item text-white" href="about.php">About Us</a>
             <a class="dropdown-item text-white" href="term.php">Terminology</a>
             <a class="dropdown-item text-white" href="contact-us.php">Contact Us</a>
@@ -41,7 +41,7 @@ echo '
     
     <div class="form-inline">
       <input class="form-control" type="text" placeholder="Search for City"  id ="city"  name = "city">
-      <button class="btn btn-primary my-2 my-sm-0" type="submit" onclick="searchCity()"><i class="fa fa-search" ></i></button>
+      <button class="btn search-btn btn-primary my-2 my-sm-0" type="submit" onclick="searchCity()"><i class="fa fa-search" ></i></button>
     </div>
     </div>
     </nav>
