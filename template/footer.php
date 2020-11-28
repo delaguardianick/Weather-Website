cf9e6a8b6517077e956b907dab0b5c04070241de
delaguardianick <delaguardianick@gmail.com>
2020-11-28 12:20:44 +0100

    added footer and updated nav

---
+++ template/footer.php
@@ -0,0 +1,10 @@
+<?php
+echo '
+    <footer class="footer">
+        <div class="">
+            <span class="float-left mt-3 ml-3">Weather<br>Group-10</span>
+            <span class="float-right mt-3 mr-3">Sagar<br>Andrew<br>David<br>Nicholas</span>
+        </div>
+    </footer>
+';
+?>
--- template/navigation.php
+++ template/navigation.php
@@ -1,6 +1,6 @@
 <?php 
 echo '
-<nav class="navbar navbar-expand-lg navbar-light text-white d-flex" style="background:#1D3557; ">
+<nav class="navbar navbar-expand-lg navbar-light text-white d-flex">
     <a class="navbar-brand text-white" href="#"> <img src="https://images-na.ssl-images-amazon.com/images/I/61nuuPxUvaL.png" alt="" id="logo"> Weather</a>
     <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -11,37 +11,37 @@ echo '
           <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Forecast
           </a>
-          <div style="background:#264672;" class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item text-white" href="#">Current</a>
-            <a class="dropdown-item text-white" href="#">Minutely</a>
-            <a class="dropdown-item text-white" href="#">Hourly</a>
-            <a class="dropdown-item text-white" href="#">Daily</a>
-            <a class="dropdown-item text-white" href="#">Historical</a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item text-white" href="current.php">Current</a>
+            <a class="dropdown-item text-white" href="minute.php">Minutely</a>
+            <a class="dropdown-item text-white" href="hourly.php">Hourly</a>
+            <a class="dropdown-item text-white" href="daily.php">Daily</a>
+            <a class="dropdown-item text-white" href="historical.php">Historical</a>
           </div>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Info</a>
-          <div style="background:#264672; " class="  text-white dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item text-white" href="#">About Us</a>
-            <a class="dropdown-item text-white" href="#">Terminology</a>
-            <a class="dropdown-item text-white" href="#">Contact Us</a>
+          <div class="  text-white dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item text-white" href="about.php">About Us</a>
+            <a class="dropdown-item text-white" href="term.php">Terminology</a>
+            <a class="dropdown-item text-white" href="contact-us.php">Contact Us</a>
           </div>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white" href="#">Alerts</a>
+          <a class="nav-link text-white" href="alerts.php">Alerts</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white" href="#">Playlists</a>
+          <a class="nav-link text-white" href="spotify.php">Playlists</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white" href="#">City Comparison</a>
+          <a class="nav-link text-white" href="comparison.php">City Comparison</a>
         </li>
         
       </ul>
     
     <div class="form-inline">
       <input class="form-control" type="text" placeholder="Search for City"  id ="city"  name = "city">
-      <button class="btn btn-primary my-2 my-sm-0" type="submit" onclick="searchCity()"><i class="fa fa-search" ></i></button>
+      <button class="btn search-btn btn-primary my-2 my-sm-0" type="submit" onclick="searchCity()"><i class="fa fa-search" ></i></button>
     </div>
     </div>
     </nav>
